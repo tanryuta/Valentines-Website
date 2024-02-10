ad6c03b94624f4b9ae5b06567dc34448b40e4a12
tanryuta <ryutainoyb@gmail.com>
2024-02-09 18:24:41 -0800

    aaaa

--- main.js
+++ main.js
@@ -4,10 +4,6 @@ const jpg = document.querySelector(".jpg");
 const yesbtn = document.querySelector(".yes-btn");
 const nobtn = document.querySelector(".no-btn");
 
-yesbtn.addEventListener("click", () => {
-    question.innerHTML = "See you on February 14th! :3"
-    jpg.scr = "second.gif"
-}) 
 
 nobtn.addEventListener("mouseover", () =>{
     const nobtnRect = nobtn.getBoundingClientRect();
