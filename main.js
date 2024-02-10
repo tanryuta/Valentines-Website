b712b464c575e28d8adf1058d8b465ff47f574fa
tanryuta <ryutainoyb@gmail.com>
2024-02-09 18:36:16 -0800

    123

--- main.js
+++
@@ -1,5 +0,0 @@
-const wrappper = document.querySelector(".wrapper");
-const question = document.querySelector(".question");
-const jpg = document.querySelector(".jpg");
-const yesbtn = document.querySelector(".yes-btn");
-const nobtn = document.querySelector(".no-btn");
