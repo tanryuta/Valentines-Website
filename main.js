c2c88982c8af6d99dc0b56b4a03debbc9c22b692
tanryuta <ryutainoyb@gmail.com>
2024-02-09 18:29:01 -0800

    aaa

--- main.js
+++ main.js
@@ -3,16 +3,3 @@ const question = document.querySelector(".question");
 const jpg = document.querySelector(".jpg");
 const yesbtn = document.querySelector(".yes-btn");
 const nobtn = document.querySelector(".no-btn");
-
-
-nobtn.addEventListener("mouseover", () =>{
-    const nobtnRect = nobtn.getBoundingClientRect();
-    const maxX = window.innerWidth - nobtnRect.width;
-    const maxY = window.innerHeight - nobtnRect.height;
-
-    const randomX = Math.floor(Math.random() * maxX);
-    const randomY = Math.floor(Math.random() * maxY);
-
-    nobtn.computedStyleMap.left = randomX + "px";
-    nobtn.computedStyleMap.top = randomY + "px";
-})
